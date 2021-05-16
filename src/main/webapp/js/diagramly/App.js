ea40aa3c51d5e9cc0325fe26edc24e92df187ed2
liew312 <liewzipeng312@hotmail.com>
2021-05-17 00:24:22 +0800

    testing disable sharebutton

--- src/main/webapp/js/diagramly/App.js
+++ src/main/webapp/js/diagramly/App.js
@@ -5725,41 +5725,41 @@ App.prototype.updateButtonContainer = function()
 			{
 				if (this.shareButton == null)
 				{
-					this.shareButton = document.createElement('div');
-					this.shareButton.className = 'geBtn gePrimaryBtn';
-					this.shareButton.style.display = 'inline-block';
-					this.shareButton.style.backgroundColor = '#F2931E';
-					this.shareButton.style.borderColor = '#F08705';
-					this.shareButton.style.backgroundImage = 'none';
-					this.shareButton.style.padding = '2px 10px 0 10px';
-					this.shareButton.style.marginTop = '-10px';
-					this.shareButton.style.height = '28px';
-					this.shareButton.style.lineHeight = '28px';
-					this.shareButton.style.minWidth = '0px';
-					this.shareButton.style.cssFloat = 'right';
-					this.shareButton.setAttribute('title', mxResources.get('share'));
+					// this.shareButton = document.createElement('div');
+					// this.shareButton.className = 'geBtn gePrimaryBtn';
+					// this.shareButton.style.display = 'inline-block';
+					// this.shareButton.style.backgroundColor = '#F2931E';
+					// this.shareButton.style.borderColor = '#F08705';
+					// this.shareButton.style.backgroundImage = 'none';
+					// this.shareButton.style.padding = '2px 10px 0 10px';
+					// this.shareButton.style.marginTop = '-10px';
+					// this.shareButton.style.height = '28px';
+					// this.shareButton.style.lineHeight = '28px';
+					// this.shareButton.style.minWidth = '0px';
+					// this.shareButton.style.cssFloat = 'right';
+					// this.shareButton.setAttribute('title', mxResources.get('share'));
 					
-					var icon = document.createElement('img');
-					icon.setAttribute('src', this.shareImage);
-					icon.setAttribute('align', 'absmiddle');
-					icon.style.marginRight = '4px';
-					icon.style.marginTop = '-3px';
-					this.shareButton.appendChild(icon);
+					// var icon = document.createElement('img');
+					// icon.setAttribute('src', this.shareImage);
+					// icon.setAttribute('align', 'absmiddle');
+					// icon.style.marginRight = '4px';
+					// icon.style.marginTop = '-3px';
+					// this.shareButton.appendChild(icon);
 					
-					if (uiTheme != 'dark' && uiTheme != 'atlas')
-					{
-						this.shareButton.style.color = 'black';
-						icon.style.filter = 'invert(100%)';
-					}
+					// if (uiTheme != 'dark' && uiTheme != 'atlas')
+					// {
+					// 	this.shareButton.style.color = 'black';
+					// 	icon.style.filter = 'invert(100%)';
+					// }
 					
-					mxUtils.write(this.shareButton, mxResources.get('share'));
+					// mxUtils.write(this.shareButton, mxResources.get('share'));
 					
-					mxEvent.addListener(this.shareButton, 'click', mxUtils.bind(this, function()
-					{
-						this.actions.get('share').funct();
-					}));
+					// mxEvent.addListener(this.shareButton, 'click', mxUtils.bind(this, function()
+					// {
+					// 	this.actions.get('share').funct();
+					// }));
 					
-					this.buttonContainer.appendChild(this.shareButton);
+					// this.buttonContainer.appendChild(this.shareButton);
 				}
 			}
 			else if (this.shareButton != null)
